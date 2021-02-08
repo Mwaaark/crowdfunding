d0552ab86d4062f28d8b2f278b3c18552695b8e0
Mark Robert Bumatay <kramtrebor1995@gmail.com>
2021-02-08 19:30:34 +0800

    ready to try deploying

--- app.js
+++ app.js
@@ -12,9 +12,12 @@ const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const User = require("./models/user");
 const dayjs = require("dayjs");
 const relativeTime = require("dayjs/plugin/relativeTime");
+const User = require("./models/user");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const MongoStore = require("connect-mongo")(session);
 
 const userRoutes = require("./routes/users");
 const projectRoutes = require("./routes/projects");
@@ -22,7 +25,11 @@ const commentRoutes = require("./routes/comments");
 const donationRoutes = require("./routes/donations");
 const adminRoutes = require("./routes/admin");
 
-mongoose.connect("mongodb://localhost:27017/crowdfunding", {
+const dbUrl = "mongodb://localhost:27017/crowdfunding";
+// const dbUrl = process.env.DB_URL;
+// "mongodb://localhost:27017/crowdfunding"
+
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
@@ -44,22 +51,78 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+  })
+);
+
+const secret = process.env.SESSION_SECRET;
+
+const store = new MongoStore({
+  url: dbUrl,
+  secret,
+  touchAfter: 24 * 60 * 60,
+});
 
 dayjs.extend(relativeTime);
 app.locals.dayjs = require("dayjs");
 
 const sessionConfig = {
-  secret: "thisshouldbeabettersecret!",
+  store,
+  name: "session",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    // secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet({ contentSecurityPolicy: false }));
+
+const scriptSrcUrls = [
+  "https://code.jquery.com",
+  "https://cdn.jsdelivr.net",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.datatables.net",
+];
+const styleSrcUrls = [
+  "https://cdn.jsdelivr.net",
+  "https://use.fontawesome.com/",
+  "https://fonts.googleapis.com",
+  "https://cdn.datatables.net",
+];
+const connectSrcUrls = [];
+const fontSrcUrls = [
+  "https://fonts.gstatic.com",
+  "https://use.fontawesome.com/",
+];
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", "blob:"],
+      objectSrc: [],
+      imgSrc: [
+        "'self'",
+        "blob:",
+        "data:",
+        "https://res.cloudinary.com/dzfkuznwb/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+        "https://images.unsplash.com/",
+      ],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -86,7 +149,7 @@ app.use("/projects/:id/donations", donationRoutes);
 app.use("/admin", adminRoutes);
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.redirect("/projects");
 });
 
 app.all("*", (req, res, next) => {
